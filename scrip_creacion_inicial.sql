a86e105669f6dc0717fb12e8e139ec9fc6aefbdf
Lucas Visser <lucasnvp@gmail.com>
2016-10-22 00:42:49 -0300

    -Mas tablas

--- scrip_creacion_inicial.sql
+++ scrip_creacion_inicial.sql
@@ -26,8 +26,7 @@ create table [SELECTIONADOS].[Afiliados](
 create table [SELECTIONADOS].[Estado_Civil](
   Id_Estado_Civil INT PRIMARY KEY IDENTITY(1,1),
   Descripcion VARCHAR(255),
-)
-GO
+)GO
 
 INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Casado')
 INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Casada')
@@ -38,4 +37,61 @@ INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Viuda')
 INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Divorsiado')
 INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Divorsiada')
 GO
+
+-- Creamos la nueva Tabla de Planes Medicos
+CREATE TABLE [SELECTIONADOS].[Planes](
+  Id_Plan INT PRIMARY KEY IDENTITY(1,1),
+  Descripcion VARCHAR(255),
+)GO
+
+--Creamos la tabla de Cuotas
+CREATE TABLE [SELECTIONADOS].[Cuotas](
+  Id_Cuota INT PRIMARY KEY IDENTITY(1,1),
+  Nro_Afiliado INT,
+  monto money,
+)GO
+
+-- Creamos la tabla Bajas, se utiliza para registrar el evento de baja de un afiliado
+create table [SELECTIONADOS].[Baja](
+  Id_Baja INT PRIMARY KEY IDENTITY(1,1),
+  Id_Afiliado INT,
+  Fecha_Baja DATETIME,
+  Motivo VARCHAR(255),
+)GO
+-- Crear triger para que cuando se inserte en esta tabla se tenga que dar de baja en la de afiliados
+
+-- Tabla de usuarios.
+create table [SELECTIONADOS].[Usuarios](
+  Id_Usuario INT PRIMARY KEY IDENTITY(1,1),
+  Username VARCHAR(255),
+  Password VARCHAR(255),
+)GO
+
+-- Tabla de Asignaciones de Roles y Usuarios cada usuario puede tener mas de un rol
+create table [SELECTIONADOS].[Asignacion_Rol](
+  Id_Rol INT,
+  Id_Usuario INT,
+  PRIMARY KEY (Id_Rol,Id_Usuario)
+)GO
+
+-- Tabla de Roles de Usuario
+create table [SELECTIONADOS].[Rol](
+  Id_Rol INT PRIMARY KEY IDENTITY(1,1),
+  Nombre VARCHAR(255),
+  Activo BIT,
+)GO
+
+-- Tabla de Funcionalidades
+create table [SELECTIONADOS].[Funcionalidades](
+  Id_Funcionalidad INT PRIMARY KEY IDENTITY(1,1),
+  Descripcion VARCHAR(255),
+)GO
+
+-- Creamos la tabla de roles por funcionalidad, cada rol puede tener muchas funcionalidades y una funcionalidad puede estar presente en varios roles.
+
+create table [SELECTIONADOS].[Rol_X_Funcionalidad](
+  Id_Funcionalidad INT,
+  Id_Rol INT,
+  PRIMARY KEY (Id_Funcionalidad,Id_Rol)
+)GO
 
