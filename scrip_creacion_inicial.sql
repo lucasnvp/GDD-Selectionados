d2700105b36b04f602d97cf5ee43c992b5096460
Lucas Visser <lucasnvp@gmail.com>
2016-10-23 16:31:12 -0300

    -Mas tablas

--- scrip_creacion_inicial.sql
+++ scrip_creacion_inicial.sql
@@ -1,7 +1,7 @@
 USE [GD2C2016]
 GO
 
-CREATE SCHEMA [SELECTIONADOS] AUTHORIZATION [gd]
+CREATE SCHEMA [SELECTIONADOS]
 GO
 
 -- Creamos la nueva Tabla de Afiliados
@@ -27,16 +27,6 @@ create table [SELECTIONADOS].[Estado_Civil](
   Id_Estado_Civil INT PRIMARY KEY IDENTITY(1,1),
   Descripcion VARCHAR(255),
 )GO
-
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Casado')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Casada')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Soltero')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Soltera')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Viudo')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Viuda')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Divorsiado')
-INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Divorsiada')
-GO
 
 -- Creamos la nueva Tabla de Planes Medicos
 CREATE TABLE [SELECTIONADOS].[Planes](
@@ -88,10 +78,77 @@ create table [SELECTIONADOS].[Funcionalidades](
 )GO
 
 -- Creamos la tabla de roles por funcionalidad, cada rol puede tener muchas funcionalidades y una funcionalidad puede estar presente en varios roles.
-
 create table [SELECTIONADOS].[Rol_X_Funcionalidad](
   Id_Funcionalidad INT,
   Id_Rol INT,
   PRIMARY KEY (Id_Funcionalidad,Id_Rol)
 )GO
 
+-- Tabla de logs de modificacion
+CREATE TABLE [SELECTIONADOS].[Modificación] (
+  [id_modificacion] INT NOT NULL PRIMARY KEY,
+  [numero_afiliado] INT FOREIGN KEY,
+  [fecha_modificacion] DATETIME,
+  [motivo] VARCHAR(255)
+)GO
+
+--Tabla de Profesionales
+CREATE TABLE [SELECTIONADOS].[Profesional] (
+  [matricula] INT NOT NULL PRIMARY KEY,
+  [nombre] VARCHAR(255),
+  [apellido] VARCHAR(255),
+  [tipo_doc] VARCHAR(255),
+  [nro_doc] INT,
+  [direccion] VARCHAR(255),
+  [telefono] INT,
+  [mail] VARCHAR(255),
+  [fecha_nac] DATETIME,
+  [sexo] VARCHAR(255)
+)GO
+
+--Tabla de disponibilidad profesional
+CREATE TABLE [SELECTIONADOS].[Disp_Profesional] (
+  [matricula] INT PK FK,
+  [dia] INT PK FK,
+  [hora_desde] DATETIME,
+  [hora_hasta] DATETIME,
+  [especialidad] INT
+)GO
+
+--Tabla de especializacion
+CREATE TABLE [SELECTIONADOS].[Profesional_Especializacion] (
+  [id_especialidad] INT PK,
+  [matricula] INT FK
+)GO
+
+--Tabla de especialidad
+CREATE TABLE [SELECTIONADOS].[Especialidad] (
+  [id_especialidad] INT PK,
+  [descripcion] VARCHAR(255),
+  [id_tipo_especialidad] INT FK
+)GO
+
+--Tabla de tipo de especialidad
+CREATE TABLE [SELECTIONADOS].[Tipo_especialidad] (
+  [id_tipo_especialidad] INT,
+  [descripcion] VARCHAR(255)
+)GO
+
+--Tabla de turno
+CREATE TABLE [SELECTIONADOS].[Turno] (
+  [id_turno] INT PK,
+  [id_horario] DATETIME,
+  [nro_afiliado] INT PK FK,
+  [id_especialidad] INT PK FK,
+  [matricula] INT PK FK
+)GO
+
+-- Se empiezan a insertar los valosres por defecto en las tablas
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Casado')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Casada')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Soltero')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Soltera')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Viudo')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Viuda')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Divorsiado')
+INSERT INTO [SELECTIONADOS].[Estado_Civil]([Descripcion]) VALUES ('Divorsiada')
