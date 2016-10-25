c3d5410dafbe7803e32ac3c928cfb78450a4ce77
lucasnvp <lucasnvp@gmail.com>
2016-10-25 14:14:47 -0300

    Delete scrip_pruebas.sql
    Pasa a development

--- scrip_pruebas.sql
+++
@@ -1,7 +0,0 @@
---Drops
-DROP SCHEMA SELECTIONADOS;
-DROP PROCEDURE [SELECTIONADOS].[migracionDeDatos];
-DROP TABLE [SELECTIONADOS].[Afiliados];
-
---Consultas
-SELECT Paciente_Nombre FROM gd_esquema.Maestra WHERE Paciente_Nombre IS NOT NULL;
